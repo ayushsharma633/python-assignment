c4f97cda0bb3321060f5c29622f9e0ec1c2993f2
ayushsharma633 <ayushsharma633@gmail.com>
2025-08-24 02:27:12 +0530

    Add files via upload

---
+++ Projects/Simple_Program.py
@@ -0,0 +1,16 @@
+a = float(input("Enter the first number: "))
+b= float(input("Enter the second number: "))
+
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("\nResults:")
+print('addition:  ',c)
+print('Subtraction:   ',d)
+print('multiplication:',e)
+print('division:   ',f)
+
+
+
+
---
+++ Projects/Simple_Program1.py
@@ -0,0 +1,6 @@
+a=input("Enter your first name: ")
+b=input("Enter your last name: ")
+
+Fullname = a + b
+
+print('Hello,' + Fullname + '! welcome to python Program')
