3f982060147fbe7fbabdf8fa7ed469cabda787fb
ayushsharma633 <ayushsharma633@gmail.com>
2025-08-28 09:49:32 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,13 @@
+Number= int(input('Enter a number: '))
+
+if (Number % 2 == 0) :
+    print("{} is an Even Number".format(Number))
+else:
+    print("{} is an Odd Number".format(Number))
+
+Number= int(input('Enter a number: '))
+
+if (Number % 2 == 0) :
+    print("{} is an Even Number".format(Number))
+else:
+    print("{} is an Odd Number".format(Number))
---
+++ Assignment2.1.py
@@ -0,0 +1,6 @@
+Total_Sum = 0
+
+for number in range(1,51):
+    Total_Sum = Total_Sum + number
+
+print("The sum of all integers from 1 to 50 is:",Total_Sum)
